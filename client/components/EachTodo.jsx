6472a3f26549180b7e0c26aa7b48dc2829fdc907
Alex-Shatskiy <alexlev706@gmail.com>
2021-10-18 14:13:44 +1300

    bug fix

--- client/components/EachTodo.jsx
+++ client/components/EachTodo.jsx
@@ -1,40 +1,53 @@
-import React, {useState} from 'react'
-import { connect } from 'react-redux'
-import { updateTodo, deleteTodo } from '../actions'
-import Update from './Update'
+import React, { useState } from "react"
+import { connect } from "react-redux"
+import { updateTodo, deleteTodo } from "../actions"
+import Update from "./Update"
 
-
-function EachTodo(props){
-
+const EachTodo = (props) => {
   const [doubleClickEdit, setDoubleClickEdit] = useState(false)
 
-  const updateTodoList =(todo)=>{
+  const updateTodoList = (todo) => {
     let check = todo
-    check.completed = check.completed ? 0:1
+    check.completed = check.completed ? 0 : 1
     props.dispatch(updateTodo(check))
   }
 
-  const removeTodo=(todo)=>{
+  const removeTodo = (todo) => {
     props.dispatch(deleteTodo(todo))
   }
 
-  function onDoubleClick(){
+  function onDoubleClick() {
     setDoubleClickEdit(!doubleClickEdit)
-}
+  }
 
-    return (
-      <>
-      <li className={props.todo.completed ? 'completed' : ''}>
-        <div className='view'>
-         <input className="toggle" type="checkbox" checked={props.todo.completed}  onChange={() => updateTodoList(props.todo)}/>
-         {
-         !doubleClickEdit? <label onDoubleClick={()=> onDoubleClick()}>{props.todo.todo}</label>: 
-          <Update todo={props.todo.todo} fullTodo={props.todo} enter={onDoubleClick}/>
-         }
-         <button className="destroy" onClick={()=> removeTodo(props.todo)}></button>
+  return (
+    <>
+      <li className={props.todo.completed ? "completed" : ""}>
+        <div className="view">
+          <input
+            className="toggle"
+            type="checkbox"
+            checked={props.todo.completed}
+            onChange={() => updateTodoList(props.todo)}
+          />
+          {!doubleClickEdit ? (
+            <label onDoubleClick={() => onDoubleClick()}>
+              {props.todo.todo}
+            </label>
+          ) : (
+            <Update
+              todo={props.todo.todo}
+              fullTodo={props.todo}
+              enter={onDoubleClick}
+            />
+          )}
+          <button
+            className="destroy"
+            onClick={() => removeTodo(props.todo)}
+          ></button>
         </div>
-        </li>   
-      </>
-    )
+      </li>
+    </>
+  )
 }
 export default connect()(EachTodo)
--- client/reducers/todoList.js
+++ client/reducers/todoList.js
@@ -1,48 +1,51 @@
-const reducer = (state = [], action) =>{
-  let newState 
-    switch(action.type){
+const reducer = (state = [], action) => {
+  let newState
+  switch (action.type) {
+    case "GET_TODO":
+      return action.todo
 
-      case 'GET_TODO':
-        return action.todo
+    case "ADD_TODO":
+      newState = [...state]
+      let newTodo = newState.push(action.todo)
+      return newState
 
-      case 'ADD_TODO':
-        newState = [...state]
-        let newTodo = newState.push(action.todo)
-        return newState
+    case "CHECK_TODO":
+      newState = [...state]
+      let checktodo = newState.filter((todos) => {
+        return todos.id == action.todo.id
+      })
+      checktodo.completed = action.completed
+      return newState
 
-      case 'CHECK_TODO':
-        newState = [...state]
-        let checktodo = newState.filter(todos =>{
-          return todos.id == action.todo.id
-        })
-        checktodo.completed = action.completed
-        return newState
+    case "DEL_TODO":
+      newState = [...state]
+      let deleteTodo = newState.filter((todos) => {
+        return todos.todo != action.todo.todo
+      })
+      return deleteTodo
 
-        case 'DEL_TODO':
-          newState = [...state]
-          let deleteTodo = newState.filter(todos =>{
-            return todos.todo != action.todo.todo
+    case "DEL_ALL_TODO":
+      newState = [...state]
+      let deleteAllTodo = newState.filter((todos) => {
+        return (
+          todos.completed ==
+          action.todo.filter((complete) => {
+            return complete == true
           })
-          return deleteTodo
+        )
+      })
+      return deleteAllTodo
+    case "UPDATE_TODO":
+      newState = [...state]
+      let changeTodo = newState.filter((todos) => {
+        return todos.id == action.todo.id
+      })
+      changeTodo.todo = action.todo
+      return newState
 
-        case 'DEL_ALL_TODO':
-          newState = [...state]
-          let deleteAllTodo = newState.filter(todos =>{
-            return todos.completed == action.todo.filter(complete =>{
-              return complete == true
-            })
-          })
-        case 'UPDATE_TODO':
-          newState =[...state]
-          let changeTodo = newState.filter(todos =>{
-            return todos.id == action.todo.id
-          })
-          changeTodo.todo = action.todo
-          return newState
-
-      default:
-          return state
-    }
+    default:
+      return state
+  }
 }
 
 export default reducer
